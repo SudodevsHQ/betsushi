0d70318b51db717d072b62ce56ca07ba55e17fcb
Anurag Patil <anurag.patil@headout.com>
2021-12-02 20:53:26 +0530

    feat: initialize db from docker

---
+++ schema/init.sql
@@ -0,0 +1,44 @@
+create table if not exists users
+(
+    id         serial primary key,
+    full_name  varchar,
+    currency   varchar,
+    created_at timestamp default now()
+);
+
+create table if not exists account
+(
+    id         serial primary key,
+    balance    double precision,
+    contact_id varchar,
+    user_id    int,
+    created_at timestamp default now(),
+    constraint fk_account_users foreign key (user_id) references users (id)
+
+);
+
+create table if not exists upi
+(
+    id         varchar primary key,
+    user_id    int,
+    created_at timestamp default now(),
+    constraint fk_upi_user foreign key (user_id) references users (id)
+);
+
+create type transaction_type as enum ('send', 'receive');
+create type transaction_status as enum ('queued', 'pending', 'rejected', 'processing', 'processed', 'cancelled', 'reversed');
+
+create table if not exists transaction
+(
+    id              serial primary key,
+    razorpay_tid    varchar,
+    amount          double precision,
+    user_id         int,
+    type            transaction_type,
+    fund_account_id varchar null,
+    upi             varchar,
+    status          transaction_status,
+    created_at      timestamp default now(),
+    constraint fk_transaction_users foreign key (user_id) references users (id)
+
+);
