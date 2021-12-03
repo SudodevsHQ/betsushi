66ea1bf30395acaebd5c4f6a5585a2312f42f4ad
AayushDongre <aayushdongre4@gmail.com>
2021-12-03 15:26:51 +0530

    feat: create contact and user flow

--- src/extensions/__init__.py
+++ src/extensions/__init__.py
@@ -2,10 +2,7 @@ from aiohttp.client import ClientSession
 from src.models.request.razorpayx import CreateContactRequest
 from src.extensions import razorpayx
 
-from src.database.models.users import User
-
 
 async def create_contact(session: ClientSession, data: CreateContactRequest):
     contact = await razorpayx.create_contact(session, data)
-    await User.create(full_name=contact.name, currency="USD")
     return contact
---
+++ src/routes/create_user.py
@@ -0,0 +1,22 @@
+from src.models.request.razorpayx import CreateContactRequest
+from starlette.responses import JSONResponse
+
+from src.extensions import create_contact
+from src.utils.http import session
+from src.database.models.users import User
+
+
+async def create_user(request):
+    json = await request.json()
+    request = CreateContactRequest(
+        name=json.get("name"),
+        contact=json.get("contact"),
+        email=json.get("email"),
+        type=json.get("type"),
+        reference_id=json.get("reference_id"),
+        notes=json.get("notes"),
+    )
+    contact = await create_contact(session=session, data=request)
+    await User.create(full_name=contact.name, currency="USD")
+    
+    return JSONResponse(contact.__dict__)
