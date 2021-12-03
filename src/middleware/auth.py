8b49c20f7143f5c8fb8af0131a814d97eea021b6
Anurag Patil <anurag.patil@headout.com>
2021-12-03 18:03:01 +0530

    feat: firebase auth in middleware

--- src/middleware/auth.py
+++ src/middleware/auth.py
@@ -1,10 +1,26 @@
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import JSONResponse
+from src.utils.auth import firebase_app
+from firebase_admin import auth
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
     def __init__(self, app, *args, **kwargs):
         super().__init__(app, *args, **kwargs)
+        self.app = firebase_app
 
     async def dispatch(self, request, call_next):
         response = await call_next(request)
+        id_token = request.headers.get('Authorization')
+        if not id_token:
+            return JSONResponse(
+                {'error': 'No token provided'}, status_code=401
+            )
+
+        decoded_token = auth.verify_id_token(id_token, app=self.app)
+        uid = decoded_token['uid']
+        if not uid:
+            return JSONResponse(
+                {'error': 'Invalid token'}, status_code=401
+            )
         return response
---
+++ src/utils/auth.py
@@ -0,0 +1,3 @@
+import firebase_admin
+
+firebase_app = firebase_admin.initialize_app()
