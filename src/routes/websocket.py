30341635da872f93b8cc6186805b7c11443dfb03
AayushDongre <aayushdongre4@gmail.com>
2021-12-04 19:30:31 +0530

    feat: send all transactions and balane on socket connect

--- src/routes/websocket.py
+++ src/routes/websocket.py
@@ -1,6 +1,8 @@
 from starlette.endpoints import WebSocketEndpoint
 from starlette.websockets import WebSocket, WebSocketState
 import json
+from src.utils.transactions import get_all_transactions
+from src.database.models.account import Account
 
 
 class ClientWebsocketEndpoint(WebSocketEndpoint):
@@ -11,7 +13,17 @@ class ClientWebsocketEndpoint(WebSocketEndpoint):
         await websocket.accept()
         user_id = websocket.query_params['user_id']
         self.user_socket_map[user_id] = websocket
-
+        
+        transactions = await get_all_transactions(user_id)
+        account = await Account.get_by_user_id(user_id)
+        websocket_response = {
+            "user_id": user_id,
+            "transactions": transactions,
+            "balance": float(account.balance)
+        }
+        await websocket.send_json(websocket_response)
+        
+        
     async def on_receive(self, websocket: WebSocket, data):
         user_id = json.loads(data).get('user_id')
         websocket = self.user_socket_map.get(user_id)
